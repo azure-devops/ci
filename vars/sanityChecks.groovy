ec4eee9c8ad59b1e925c63074444b104cf01d755
Claudiu Guiman <clguiman@microsoft.com>
2017-03-06 09:58:58 -0800

    Add timeout in sanity checks

--- vars/sanityChecks.groovy
+++ vars/sanityChecks.groovy
@@ -4,31 +4,33 @@ def linux_build() {
 def windows_build() {
     bat 'echo \'works\''
 }
-stage('Verify VMAgents templates') {
-    parallel 'Custom Ubuntu without init script': {
-        node('s-cust-no-init-ubuntu') {
-            linux_build()
-        }
-    },
-    /*'Custom Ubuntu with init script': {
-        node('s-cust-init-ubuntu') {
-            linux_build()
-        }
-    },*/
-    'Reference Ubuntu with init script': {
-        node('s-ref-ubuntu') {
-            linux_build()
-        }
-    },
-    'Custom Windows with init script': {
-        node('s-cust-windows') {
-            windows_build()
-        }
-    },
-    'Reference Windows with init script': {
-        node('s-ref-windows') {
-            windows_build()
-        }
-    },
-    failFast: true
+timeout(60) {
+    stage('Verify VMAgents templates') {
+        parallel 'Custom Ubuntu without init script': {
+            node('s-cust-no-init-ubuntu') {
+                linux_build()
+            }
+        },
+        /*'Custom Ubuntu with init script': {
+            node('s-cust-init-ubuntu') {
+                linux_build()
+            }
+        },*/
+        'Reference Ubuntu with init script': {
+            node('s-ref-ubuntu') {
+                linux_build()
+            }
+        },
+        'Custom Windows with init script': {
+            node('s-cust-windows') {
+                windows_build()
+            }
+        },
+        'Reference Windows with init script': {
+            node('s-ref-windows') {
+                windows_build()
+            }
+        },
+        failFast: true
+    }
 }
