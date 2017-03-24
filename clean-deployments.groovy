f148c7d4c429327878bc3a5dfbd037a7a9fe24f7
Eric Jizba <erijiz@microsoft.com>
2017-03-24 14:04:37 -0700

    Change private build url to public version

--- clean-deployments.groovy
+++ clean-deployments.groovy
@@ -14,11 +14,12 @@ node('spin-k8s-test') {
             }
         }
     } catch (e) {
+        def public_build_url = "$BUILD_URL".replaceAll("10.0.0.4:8080" , "devops-ci.westcentralus.cloudapp.azure.com")
         withCredentials([string(credentialsId: 'TeamEmailAddress', variable: 'email_address')]) {
             emailext (
                 attachLog: true,
                 subject: "Jenkins Job '$JOB_NAME' #$BUILD_NUMBER Failed",
-                body: "$BUILD_URL",
+                body: public_build_url,
                 to: env.email_address
             )
         }
