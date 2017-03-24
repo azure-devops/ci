42b955ff8370487ff0ce5e7a72558807f6ab63e2
Eric Jizba <erijiz@microsoft.com>
2017-03-24 12:05:01 -0700

    Add daily schedule and email notifications on failures

--- clean-deployments.groovy
+++ clean-deployments.groovy
@@ -1,16 +1,27 @@
 node('spin-k8s-test') {
-    properties([parameters([
-            string(defaultValue: '72f988bf-86f1-41af-91ab-2d7cd011db47', description: 'Tenant id', name: 'tenant_id'),
-            string(defaultValue: 'AzDevOpsTestingSP', description: 'Credentials id', name: 'cred_id')
-            ])])
+    try {
+        properties([
+            pipelineTriggers([cron('@daily')])
+        ])
 
-    checkout scm
+        checkout scm
 
-    stage('Delete deployments') {
-        def script_path = 'scripts/clean-deployments.sh'
-        sh 'sudo chmod +x ' + script_path
-        withCredentials([usernamePassword(credentialsId: params.cred_id, passwordVariable: 'client_secret', usernameVariable: 'client_id')]) {
-            sh script_path + ' -ci ' + env.client_id + ' -cs ' + env.client_secret + ' -ti ' + params.tenant_id
+        stage('Delete deployments') {
+            def script_path = 'scripts/clean-deployments.sh'
+            sh 'sudo chmod +x ' + script_path
+            withCredentials([usernamePassword(credentialsId: 'AzDevOpsTestingSP', passwordVariable: 'client_secret', usernameVariable: 'client_id')]) {
+                sh script_path + ' -ci ' + env.client_id + ' -cs ' + env.client_secret
+            }
         }
+    } catch (e) {
+        withCredentials([string(credentialsId: 'TeamEmailAddress', variable: 'email_address')]) {
+            emailext (
+                attachLog: true,
+                subject: "Jenkins Job '$JOB_NAME' #$BUILD_NUMBER Failed",
+                body: "$BUILD_URL",
+                to: env.email_address
+            )
+        }
+        throw e
     }
 }
