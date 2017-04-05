05b5dd62d1f4391b026f7a6d72c34d38a35e8a3b
Claudiu Guiman <clguiman@microsoft.com>
2017-04-05 13:28:54 -0700

    Added Jenkins VMAgent init scripts

---
+++ scripts/init-ubuntu-dev-agent.sh
@@ -0,0 +1,16 @@
+set -x
+
+#Install Java
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-7-jdk
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update --fix-missing
+
+# Install git
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git
+
+#Install Maven
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y maven
+
+#make sure we can build Jenkins plugins
+mkdir ~/.m2
+wget https://raw.githubusercontent.com/azure-devops/ci/master/resources/jenkins-settings.xml -O ~/.m2/settings.xml
---
+++ scripts/init-windows-dev-agent.ps1
@@ -0,0 +1,53 @@
+Set-ExecutionPolicy Unrestricted
+$jenkinsserverurl = $args[0]
+$vmname = $args[1]
+$secret = $args[2]
+
+#make sure we can build Jenkins plugins
+mkdir c:\.m2
+$wcs = New-Object System.Net.WebClient
+$wcs.DownloadFile("https://raw.githubusercontent.com/azure-devops/ci/master/resources/jenkins-settings.xml", "c:\.m2\settings.xml")
+
+Set-Location d:\
+
+# Download the file to a specific location
+Write-Output "Downloading zulu SDK "
+$source = "http://azure.azulsystems.com/zulu/zulu1.7.0_51-7.3.0.4-win64.zip?jenkins"
+mkdir d:\azurecsdir
+$destination = "d:\azurecsdir\zuluJDK.zip"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($source, $destination)
+
+Write-Output "Unzipping JDK "
+# Unzip the file to specified location
+$shell_app = new-object -com shell.application
+$zip_file = $shell_app.namespace($destination)
+mkdir d:\java
+$destination = $shell_app.namespace("d:\java")
+$destination.Copyhere($zip_file.items())
+Write-Output "Successfully downloaded and extracted JDK "
+
+# Downloading jenkins slaves jar
+Write-Output "Downloading jenkins slave jar "
+$slaveSource = $jenkinsserverurl + "jnlpJars/slave.jar"
+$destSource = "d:\java\slave.jar"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($slaveSource, $destSource)
+
+# execute agent
+Write-Output "Executing agent process "
+$java = "d:\java\zulu1.7.0_51-7.3.0.4-win64\bin\java.exe"
+$jar = "-jar"
+$jnlpUrl = "-jnlpUrl"
+$secretFlag = "-secret"
+$serverURL = $jenkinsserverurl + "computer/" + $vmname + "/slave-agent.jnlp"
+while ($true) {
+  try {
+    # Launch
+    & $java -jar $destSource $secretFlag  $secret $jnlpUrl $serverURL -noReconnect
+  }
+  catch [System.Exception] {
+    Write-Output $_.Exception.ToString()
+  }
+  Start-Sleep 10
+}
