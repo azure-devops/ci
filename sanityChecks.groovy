65cb2b9a0abaf772fb40e5cebe73075ffd31d37d
Claudiu Guiman <clguiman@microsoft.com>
2017-04-06 11:08:08 -0700

    Add sanity checks for agents with private IPs

--- sanityChecks.groovy
+++ sanityChecks.groovy
@@ -8,21 +8,33 @@ pipeline {
         stage('Verify VMAgents templates') {
             steps {
                  parallel ( failFast: true,
-                    'Custom Ubuntu without init script': {
-                        runSanityChecks('s-cust-no-init-ubuntu')
+                    'Custom Ubuntu without init script (public IP)': {
+                        runSanityChecks('s-cust-no-init-ubuntu-public')
                     },
-                    /* Temporarily disable 'Custom Ubuntu with init script' because the vm agent template is not configured correctly.
+                    /* Temporarily disable 'Custom Ubuntu with init script (public IP)' because the vm agent template is not configured correctly.
                     'Custom Ubuntu with init script': {
-                        runSanityChecks('s-cust-init-ubuntu')
+                        runSanityChecks('s-cust-init-ubuntu-public')
                     },*/
-                    'Reference Ubuntu with init script': {
-                        runSanityChecks('s-ref-ubuntu')
+                    'Reference Ubuntu with init script (public IP)': {
+                        runSanityChecks('s-ref-ubuntu-public')
                     },
-                    'Custom Windows with init script': {
-                        runSanityChecks('s-cust-windows')
+                    'Custom Windows with init script (public IP)': {
+                        runSanityChecks('s-cust-windows-public')
                     },
-                    'Reference Windows with init script': {
-                        runSanityChecks('s-ref-windows')
+                    'Reference Windows with init script (public IP)': {
+                        runSanityChecks('s-ref-windows-public')
+                    },
+                    'Custom Ubuntu without init script (private IP)': {
+                        runSanityChecks('s-cust-no-init-ubuntu-private')
+                    },
+                    'Reference Ubuntu with init script (private IP)': {
+                        runSanityChecks('s-ref-ubuntu-private')
+                    },
+                    'Custom Windows with init script (private IP)': {
+                        runSanityChecks('s-cust-windows-private')
+                    },
+                    'Reference Windows with init script (private IP)': {
+                        runSanityChecks('s-ref-windows-private')
                     })
             }
         }
