613c5bd1935634a9c39f97415ee91b15809c30da
Eric Jizba <erijiz@microsoft.com>
2017-05-09 11:12:24 -0700

    Remove quotes around kubeconfig path

--- scripts/deploy-quickstart-template.sh
+++ scripts/deploy-quickstart-template.sh
@@ -151,7 +151,7 @@ if [[ "$template_name" == *"k8s"* ]]; then
   >&2 echo "Copying Kubernetes credentials to the agent..."
   >&2 az acs kubernetes get-credentials --resource-group=$scenario_name --name=containerservice-$scenario_name --ssh-key-file=$temp_key_path
 
-  if [ ! -s "~/.kube/config" ]; then
+  if [ ! -s ~/.kube/config ]; then
     >&2 echo "Failed to copy kubeconfig for kubernetes cluster."
     exit -1
   fi
