b521e2da93c7f8b96ddab9a31b67c7b944d62b27
Claudiu Guiman <clguiman@microsoft.com>
2017-05-31 08:57:12 -0700

    Replace upload job with pipeline step

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -24,6 +24,7 @@ def call() {
 
                     stash includes: testResultFilePatterns.surefire + ', ' + testResultFilePatterns.findBugs, name: 'test_results'
                     sh 'cp target/*.hpi .'
+                    stash includes: '*.hpi', name: 'artifacts'
                     archiveArtifacts '*.hpi'
                 }
             }
@@ -63,14 +64,8 @@ def call() {
                 step([$class: 'FindBugsPublisher', canComputeNew: false, defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', pattern: testResultFilePatterns.findBugs, unHealthy: ''])
             }
             stage('Upload Bits') {
-                build job: 'Upload Bits',
-                    parameters: [
-                        string(name: 'parent_project', value: "${env.JOB_NAME}"),
-                        string(name: 'parent_build_number', value: "${env.BUILD_NUMBER}"),
-                        string(name: 'container_name', value: 'devops-jenkins'),
-                        string(name: 'artifacts_pattern', value: '*.hpi'),
-                        string(name: 'virtual_path', value: "${env.JOB_NAME}/${env.BUILD_NUMBER}")
-                    ]
+                unstash 'artifacts'
+                azureUpload cntPubAccess: true, containerName: 'devops-jenkins', filesPath: '*.hpi', storageCredentialId: 'devops-public-storage', virtualPath: env.JOB_NAME +'/' +env.BUILD_NUMBER
             }
         }
 
