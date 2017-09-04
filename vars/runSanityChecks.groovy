e9115c162218032433f6a48353060e8c63ea561f
Raphael Yu <xianyu@microsoft.com>
2017-09-04 15:47:16 +0800

    Fix the sanity checks.

--- vars/runSanityChecks.groovy
+++ vars/runSanityChecks.groovy
@@ -4,10 +4,6 @@ import com.microsoft.azure.devops.ci.utils
 def call(nodeLabel) {
     def ciUtils = new com.microsoft.azure.devops.ci.utils()
     node(nodeLabel) {
-        if ( ciUtils.isWindows() ) {
-            bat 'echo \'works\''
-        } else {
-            sh 'echo "works"'
-        }
+        echo 'works'
     }
 }
